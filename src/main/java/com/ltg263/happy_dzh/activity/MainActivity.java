9c2a912a99fe619c25faa1c1903eeaef512b060f
ltg_17710666165 <ltg263@126.com>
2018-03-24 16:47:01 +0800

    已完成

--- src/main/java/com/ltg263/happy_dzh/activity/MainActivity.java
+++ src/main/java/com/ltg263/happy_dzh/activity/MainActivity.java
@@ -38,6 +38,8 @@ public class MainActivity extends BaseActivity {
                         .url("http://v.juhe.cn/wepiao/query")
                         .addQuery("key", "b9028c6563dcb2cf68efeac77395341a")
                         .build();
+                //网络请求方式 默认为POST
+                params.setMethod(RxHttpParams.HttpMethod.GET);
                 RetrofitUtil.request(params, String.class, new RetrofitUtil.HttpCallBackImpl<String>() {
                     @Override
                     public void onCompleted(String contactsBean) {
